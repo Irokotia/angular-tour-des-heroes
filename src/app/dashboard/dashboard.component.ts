dbf29cc0bec6f21fa996d7c7167dee64475e426a
Irokotia <boissinrowinpro@gmail.com>
2020-04-03 23:13:30 +0200

    Correction bug

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -22,7 +22,7 @@ export class DashboardComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes.slice(0, 5));
+      .subscribe(heroes => this.heroes = heroes.slice(0, 3));
   }
 
   getWeapons(): void {
